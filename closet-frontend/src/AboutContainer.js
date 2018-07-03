0854d0b5ca04e7b4f4c2867d653da03b278b56d9
Thomas DeFeo <thomasmdefeo@gmail.com>
2018-07-03 13:25:24 -0700

    add content to aboutContainer

--- closet-frontend/src/AboutContainer.js
+++ closet-frontend/src/AboutContainer.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class AboutContainer extends Component {
   render() {
     return(
-      <div>about</div>
+      <Wrapper>
+        <h1>About Us</h1>
+        <p>
+          Closet is a clothing application that lets users use thier own custom
+          sizes as an attribute in their search for well-fitting shirts. We take the
+          guessing game out of online shopping and use our own algorithm to help match
+          you with the shirt fits best across all of our brands and sizes.
+        </p>
+      </Wrapper>
     );
   }
 }
 
 export default AboutContainer;
+
+const Wrapper = styled.div`
+  border: 1px solid black;
+  padding: 100px;
+  display: flex;
+  flex-direction: column;
+  width: 600px;
+  margin-left: 100px;
+
+  h1 {
+    display: flex;
+    justify-content: flex-start;
+  }
+  p {
+    font-size: 16px;
+    display: block;
+    justify-content: flex-start;
+    line-height: 25px;
+  }
+`;
